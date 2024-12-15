7098bca52848022d61030e2d4e7ff1c9bec6eb64
WhiredPlanck <whiredplanck@outlook.com>
2024-12-15 19:55:55 +0800

    refactor: remove librime charcode plugin
    It's deprecated by upstream

--- app/src/main/jni/cmake/Rime.cmake
+++ app/src/main/jni/cmake/Rime.cmake
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # if you want to add some new plugins, add them to librime_jni/rime_jni.cc too
-set(RIME_PLUGINS librime-lua librime-charcode librime-octagram librime-predict)
+set(RIME_PLUGINS librime-lua librime-octagram librime-predict)
 
 # symlink plugins
 foreach(plugin ${RIME_PLUGINS})
@@ -20,16 +20,14 @@ if(NOT EXISTS "${CMAKE_SOURCE_DIR}/librime/plugins/librime-lua/thirdparty")
        COPY_ON_ERROR SYMBOLIC)
 endif()
 
-# librime-charcode
-option(BUILD_WITH_ICU "" OFF)
-
 option(BUILD_TEST "" OFF)
 option(BUILD_STATIC "" ON)
 add_subdirectory(librime)
-target_compile_options(rime-static PRIVATE "-ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}=.")
+target_compile_options(
+  rime-static PRIVATE "-ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}=.")
 
-target_link_libraries(rime-charcode-objs Boost::locale)
+target_compile_options(
+  rime-lua-objs PRIVATE "-ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}=.")
 
-target_compile_options(rime-lua-objs PRIVATE "-ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}=.")
-
-target_compile_options(rime-octagram-objs PRIVATE "-ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}=.")
+target_compile_options(
+  rime-octagram-objs PRIVATE "-ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}=.")
